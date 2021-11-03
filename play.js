78c47e2031baa448140c590026d51648fcddfcdf
vagrant <arlo@gmail.com>
2021-11-03 22:08:00 +0000

    This refractored some Functions into input.js

--- play.js
+++ play.js
@@ -2,29 +2,8 @@
 // Port: 50541
 
 const {connect} = require("./client");
+const {setupInput} = require("./input");
 
-// **************  Setup stdin
-
-const handleUserInput = function (keyStroke) {
-  // your code here
-  
-  if (keyStroke === '\u0003') {
-    process.exit();
-  }
-
-};
-
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-};
 
 console.log("Connecting ...");
 connect();
